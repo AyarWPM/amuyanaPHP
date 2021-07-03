8edd19af3f3b44b7ed1115d6b88532d858cbd2a8
Ayar Portugal <ayarportugal@gmail.com>
2021-07-04 00:00:45 +0200

    index

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php $currentPage="index";include 'includes/header.php'?>
-<div class="home"><h1>welcome</h1>
-<p>This is a prototype of Amuyaña.</P>
-<p>Developed by Ayar Portugal</p>
-<p><a href="mailto:ayarportugal@gmail.com">contact</a></p>
+<div class="home">
+<div class="canvas">
+<div class="message">
+<h1>welcome</h1>
+<hr>
+<p>Amuyaña is a web Application that allows the creation of visual representations of <b><i>tables of deductions</i></b> from the Logic of the contradictory.</p>
+<p>A <b><i>Table of deduction</i></b> is the representation of a set of phenomena that form any kind of system when we postulate the Fundamental principle of the Logic of the contradictory.</p>
+<p>It is an application of the epistemological theory of Stéphane Lupasco (1951) using our modern technology. It is one of the rare existing applications that allows the creation of logic systems which are not based on the classical aristotelian principles.</p>
+<p>Contrary to classical binary logic, the Logic of the contradictory represents the flow of the thought (ideas, concepts, ...) and the phenomena itself as a cyclic processes (not linear) that develop <i>transfinitely</i> into nested levels of conceptualization following the nested <i>levels of reality</i> that the phenomena create, like fractals.</p>
+<p>The App is a computer tool that allows the formulation of theories of complex systems, for instance a social system described with the categories of the Theory of reciprocity, or a physical system of subatomic particles. In particular, it is believed that one day this application can help create models that can be read and understood by quantic processors.</p>
+<p>The App is developed by Ayar Portugal, co-founder of CREAR (Collectif de Recherche Et d'Action pour la Réciprocité), a Belgian non-profit organization which has the objective of promoting reciprocity as a systemic alternative to capitalism.</p>
+<p>In that sense, Ayar Portugal is currently using Amuyaña to create a <i>contradictorial</i> logic system encompassing the economic theories of reciprocity and exchange, providing conceptual grounds to any claim of an alternative to the current dominant system.</p>
+<p>In the future, the App could allow the recording and analysis of empirical data, have the possibility to create cooperatively Tables of deductions as to create collective theories (for instance to find solutions to pressing issues in our community), ... however Ayar is not a professional developper, he tinkered this prototype thanks to the free Internet and he needs help to continue the developpment of the App. </p>
+<p>We are still in a capitalist system and we need funds to continue with the developpment. If you feel like we should continue with this project please feel free to contribute to <a href="https://reciprocidad.org/">our association</a>.</p>
+<h3>Credits:</h3>
+<p>To many people answering questions in Stackoverflow.</p>
+<p>Channels from Youtube: Web Dev Simplified, Kevin Powell, Wes Bos, Thomas Bradley, ... </p>
+<p>Tutorials by: Jorge Montoya, guru99, css-tricks.com, betterprogramming.pub, ...</p>
+<p>External libraries: jQuery library (just a few lines).</p>
+<p>Credits mainly to my family.</p>
+<p>Feel free to test the App in the <a href="tables.php">Table of deductions page</a>. If you have any question or enquiry feel free to <a href="mailto:ayarportugal@gmail.com">contact me</a>.</p>
+<p>Ayar</p>
+<p>Last update: 3/7/2021</p>
+</div>
+</div>
 </div>
